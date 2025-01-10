eefdaacb075bd8c7941baff3925c2cd172c754d0
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2025-01-10 15:18:28 +0400

    Add stub for timestamp

--- wireclient/wireclient_test.go
+++ wireclient/wireclient_test.go
@@ -129,71 +129,90 @@ func TestConn(t *testing.T) {
 	})
 }
 
-func TestDecimal128(t *testing.T) {
+func TestTypes(t *testing.T) {
 	t.Parallel()
 
 	uri := setup(t)
 
-	d := wirebson.Decimal128{H: 13, L: 42}
-	md := bson.NewDecimal128(13, 42)
-
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	t.Cleanup(cancel)
 
-	conn := ConnectPing(ctx, uri, logger(t))
-	require.NotNil(t, conn)
+	var conn *Conn
+	var mConn *mongo.Client
 
-	err := conn.Login(ctx, "username", "password", "admin")
-	require.NoError(t, err)
+	// avoid shadowing err in subtests
+	{
+		var err error
 
-	opts := options.Client().ApplyURI(uri).SetAuth(options.Credential{Username: "username", Password: "password"})
-	mConn, err := mongo.Connect(opts)
-	require.NoError(t, err)
+		conn = ConnectPing(ctx, uri, logger(t))
+		require.NotNil(t, conn)
 
-	db := mConn.Database(t.Name())
-	require.NoError(t, db.Drop(ctx))
+		err = conn.Login(ctx, "username", "password", "admin")
+		require.NoError(t, err)
 
-	_, body, err := conn.Request(ctx, wire.MustOpMsg(
-		"insert", "test",
-		"documents", wirebson.MustArray(wirebson.MustDocument("_id", "d", "v", d)),
-		"$db", t.Name(),
-	))
-	require.NoError(t, err)
+		opts := options.Client().ApplyURI(uri).SetAuth(options.Credential{Username: "username", Password: "password"})
+		mConn, err = mongo.Connect(opts)
+		require.NoError(t, err)
 
-	doc, err := body.(*wire.OpMsg).DecodeDeepDocument()
-	require.NoError(t, err)
-	require.Equal(t, 1.0, doc.Get("ok"))
-
-	_, err = db.Collection("test").InsertOne(ctx, bson.D{{"_id", "md"}, {"v", md}})
-	require.NoError(t, err)
-
-	_, body, err = conn.Request(ctx, wire.MustOpMsg(
-		"find", "test",
-		"sort", wirebson.MustDocument("_id", int32(1)),
-		"$db", t.Name(),
-	))
-	require.NoError(t, err)
-
-	doc, err = body.(*wire.OpMsg).DecodeDeepDocument()
-	require.NoError(t, err)
-	require.Equal(t, 1.0, doc.Get("ok"))
-
-	expected := wirebson.MustArray(
-		wirebson.MustDocument("_id", "d", "v", d),
-		wirebson.MustDocument("_id", "md", "v", d),
-	)
-	require.Equal(t, expected, doc.Get("cursor").(*wirebson.Document).Get("firstBatch"))
-
-	c, err := db.Collection("test").Find(ctx, bson.D{}, options.Find().SetSort(bson.D{{"_id", 1}}))
-	require.NoError(t, err)
-
-	var res bson.A
-	err = c.All(ctx, &res)
-	require.NoError(t, err)
-
-	mExpected := bson.A{
-		bson.D{{"_id", "d"}, {"v", md}},
-		bson.D{{"_id", "md"}, {"v", md}},
+		t.Cleanup(func() {
+			require.NoError(t, conn.Close())
+			require.NoError(t, mConn.Disconnect(ctx))
+		})
 	}
-	require.Equal(t, mExpected, res)
+
+	t.Run("Decimal128", func(t *testing.T) {
+		d := wirebson.Decimal128{H: 13, L: 42}
+		md := bson.NewDecimal128(13, 42)
+
+		db := mConn.Database(t.Name())
+		require.NoError(t, db.Drop(ctx))
+
+		_, body, err := conn.Request(ctx, wire.MustOpMsg(
+			"insert", "test",
+			"documents", wirebson.MustArray(wirebson.MustDocument("_id", "d", "v", d)),
+			"$db", t.Name(),
+		))
+		require.NoError(t, err)
+
+		doc, err := body.(*wire.OpMsg).DecodeDeepDocument()
+		require.NoError(t, err)
+		require.Equal(t, 1.0, doc.Get("ok"))
+
+		_, err = db.Collection("test").InsertOne(ctx, bson.D{{"_id", "md"}, {"v", md}})
+		require.NoError(t, err)
+
+		_, body, err = conn.Request(ctx, wire.MustOpMsg(
+			"find", "test",
+			"sort", wirebson.MustDocument("_id", int32(1)),
+			"$db", t.Name(),
+		))
+		require.NoError(t, err)
+
+		doc, err = body.(*wire.OpMsg).DecodeDeepDocument()
+		require.NoError(t, err)
+		require.Equal(t, 1.0, doc.Get("ok"))
+
+		expected := wirebson.MustArray(
+			wirebson.MustDocument("_id", "d", "v", d),
+			wirebson.MustDocument("_id", "md", "v", d),
+		)
+		require.Equal(t, expected, doc.Get("cursor").(*wirebson.Document).Get("firstBatch"))
+
+		c, err := db.Collection("test").Find(ctx, bson.D{}, options.Find().SetSort(bson.D{{"_id", 1}}))
+		require.NoError(t, err)
+
+		var res bson.A
+		err = c.All(ctx, &res)
+		require.NoError(t, err)
+
+		mExpected := bson.A{
+			bson.D{{"_id", "d"}, {"v", md}},
+			bson.D{{"_id", "md"}, {"v", md}},
+		}
+		require.Equal(t, mExpected, res)
+	})
+
+	t.Run("Timestamp", func(t *testing.T) {
+		// FIXME
+	})
 }
