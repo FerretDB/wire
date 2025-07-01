887a44266c0befd237731c0bb582f3f730549a30
noisersup <patryk@kwiatek.xyz>
2025-07-01 16:18:48 +0200

    wip

---
+++ internal/util/testutil/skip.go
@@ -0,0 +1,38 @@
+// Copyright 2021 FerretDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package testutil
+
+import (
+	"flag"
+	"testing"
+)
+
+// ferretDBv1 is a flag to indicate if tests are running against FerretDB v1.
+var ferretDBv1 = flag.Bool("ferretdbv1", false, "run tests against FerretDB v1")
+
+// FlagInit initializes the flags for the test package.
+// It should be called in the TestMain function of the every package.
+func FlagInit() {
+	flag.Parse()
+}
+
+// SkipForFerretDBv1 skips the test if running against FerretDB v1.
+func SkipForFerretDBv1(tb testing.TB) {
+	tb.Helper()
+
+	if *ferretDBv1 {
+		tb.Skip("Not implemented for v1")
+	}
+}
