3cf0c2dddb8cf56a6823c3828e035e040591b452
noisersup <patryk@kwiatek.xyz>
2025-07-03 17:10:22 +0200

    wip

--- internal/util/testutil/skip.go
+++ internal/util/testutil/skip.go
@@ -22,10 +22,6 @@ import (
 // ferretDBv1 is a flag to indicate if tests are running against FerretDB v1.
 var ferretDBv1 = flag.Bool("ferretdbv1", false, "run tests against FerretDB v1")
 
-//func init() {
-//	flag.Parse()
-//}
-
 // SkipForFerretDBv1 skips the test if running against FerretDB v1.
 func SkipForFerretDBv1(tb testing.TB) {
 	tb.Helper()
